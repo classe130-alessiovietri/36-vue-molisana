82489b6f1b600eec86df003eb181522cc42bed97
Alessio Vietri <info@alessiovietri.it>
2024-09-10 12:26:35 +0200

    Completata v.1

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,225 @@
 <script>
-/* 
-  Per importare ed utilizzare un componente dentro un altro devo SEMPRE seguire questi 3 passi:
-  1) Importazione del componente
-  2) Dichiarazione del componente
-  3) Utilizzo del componente
-*/
-// 1) Importazione del componente
-import AppHeader from './components/AppHeader.vue';
-
 export default {
   data() {
-    return { 
-      count: 0
-    }
+    return {
+      mainMenuLinks: [
+        {
+          label: 'Home',
+          url: '#',
+          active: false
+        },
+        {
+          label: 'Prodotti',
+          url: '#',
+          active: true
+        },
+        {
+          label: 'Chi Siamo',
+          url: '#',
+          active: false
+        },
+        {
+          label: 'Contatti',
+          url: '#',
+          active: false
+        },
+      ],
+      footerColumnTwoLinks: [
+        {
+          label: 'Il Pastificio',
+          url: '#',
+          active: false
+        },
+        {
+          label: 'Grano',
+          url: '#',
+          active: false
+        },
+        {
+          label: 'Filiera',
+          url: '#',
+          active: false
+        },
+        {
+          label: '100 anni di pasta',
+          url: '#',
+          active: false
+        },
+        {
+          label: 'Sartoria della pasta',
+          url: '#',
+          active: false
+        },
+      ],
+      footerColumnThreeLinks: [
+        {
+          label: 'Le Classiche',
+          url: '#',
+          active: false
+        },
+        {
+          label: 'Le Integrali',
+          url: '#',
+          active: false
+        },
+        {
+          label: 'Le Speciali',
+          url: '#',
+          active: false
+        },
+        {
+          label: 'Le Biologiche',
+          url: '#',
+          active: false
+        },
+        {
+          label: 'Le Gluten-Free',
+          url: '#',
+          active: false
+        },
+      ],
+    };
   },
-  // 2) Dichiarazione del componente
   components: {
-    AppHeader
   },
   methods: {
-    incrementCount() {
-      this.count++;
-    }
   }
 }
 </script>
 
 <template>
-  <div>
-    <!-- 3) Utilizzo del componente -->
-    <AppHeader />
-    
-    <main>
-      <button @click="incrementCount()">
-        {{ count }}
-      </button>
-    </main>
-  </div>
+  <header>
+    <div class="container">
+      <div>
+        <img src="/img/la-molisana-logo.png" alt="La Molisana">
+      </div>
+
+      <nav>
+        <ul>
+          <li v-for="(link, index) in mainMenuLinks" :key="index">
+            <a :href="link.url" :class="{
+                'active': link.active
+              }">
+              {{ link.label }}
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <div class="container">
+      <div class="main-content">
+        --&gt; Content goes here &lt;--
+      </div>
+    </div>
+  </main>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-1-3">
+          <img src="/img/la-molisana-logo.png" alt="La Molisana">
+        </div>
+        <div class="col-1-3">
+          <h4>
+            Pastificio
+          </h4>
+          <ul>
+            <li v-for="(link, index) in footerColumnTwoLinks" :key="index">
+              <a :href="link.url" :class="{
+                  'active': link.active
+                }">
+                {{ link.label }}
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-1-3">
+          <h4>
+            Prodotti
+          </h4>
+          <ul>
+            <li v-for="(link, index) in footerColumnThreeLinks" :key="index">
+              <a :href="link.url" :class="{
+                  'active': link.active
+                }">
+                {{ link.label }}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
-main {
+header {
   text-align: center;
+  padding: 20px 0;
+}
+
+header ul {
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+}
+
+header ul li a {
+  text-decoration: none;
+  color: black;
+  display: inline-block;
+  padding: 10px;
+  font-weight: bold;
+  transition: all .2s ease-in-out;
+}
+
+header ul li a:hover,
+header ul li a.active {
+  background-color: #DAE9FE;
+}
+
+main {
+  background-image: url('/img/fondo-pag-speciali.jpg');
+  background-position: center;
+  background-size: cover;
+}
+
+main .main-content {
+  text-align: center;
+  padding: 50px 0;
+  color: #114674;
+  font-weight: bold;
+  font-size: 1.5rem;
+}
+
+footer {
+  padding: 40px 0 200px 0;
+  background-image: url('/img/footer-montagne-bottom.jpg');
+  background-position: bottom center;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+footer h4 {
+  text-transform: uppercase;
+  color: #114674;
+  font-size: 1.2rem;
+  margin-bottom: 20px;
+}
+
+footer ul {
+  list-style: none;
+  padding: 0;
+}
+
+footer ul li a {
+  text-decoration: none;
+  color: black;
+  display: inline-block;
 }
 </style>
